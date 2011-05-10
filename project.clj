53cd16788aa4e7501e5287adc814ec9e81f1f848
Blake Williams <blake@bendystra.ws>
2011-05-09 19:58:40 -0500

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject lotto "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :main lotto.core)
---
+++ test/lotto/test/core.clj
@@ -0,0 +1,6 @@
+(ns lotto.test.core
+  (:use [lotto.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
